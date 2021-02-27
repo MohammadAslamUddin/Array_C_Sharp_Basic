642e89ebfaa415490d05812a92c95dab0119b283
MohammadAsalmUddin <aslamuddin0015@gmail.com>
2021-02-27 15:17:35 +0600

    List<type>  Array
    count - to know the length of the list
    After declaring this Sort function this array will be sorted in ascending order
    After this function the array will give the value in descending orders
    Remove - to remove data from the array with mentioning their values
    Contains -  to find the value is in the array or not. The answer will be True or false

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Array_C_Sharp
 {
@@ -68,13 +69,46 @@ namespace Array_C_Sharp
             //myArrayList.RemoveRange(1,2);
 
             //Contains -  to find the value is in the array or not. The answer will be True or false
-            Console.WriteLine(myArrayList.Contains('C'));
+            //Console.WriteLine(myArrayList.Contains('C'));
             
-            foreach (var item in myArrayList)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in myArrayList)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
+
+
+            //List-----------------2
+
+            List<int> mylist = new List<int>();
+            mylist.Add(12);
+            mylist.Add(14);
+            mylist.Add(13);
+            mylist.Add(18);
+            mylist.Add(45);
+            mylist.Add(47);
+            mylist.Add(74);
+            mylist.Add(45);
+
+            //count - to know the length of the list
+            //Console.WriteLine(mylist.Count);
+
+            //After declaring this Sort function this array will be sorted in ascending order
+            mylist.Sort();
+
+            //After this function the array will give the value in descending orders
+            mylist.Reverse();
+
+            //Remove - to remove data from the array with mentioning their values
+            Console.WriteLine(mylist.Remove(10));
+
+            //Contains -  to find the value is in the array or not. The answer will be True or false
+            Console.WriteLine(mylist.Contains(12));
+
+            //foreach (var i in mylist)
+            //{
+            //    Console.WriteLine(i);
+            //}
 
         }
     }
